e6e05dc6cddce6bc0a8c56c6d45b9a03ecdb4f46
Angela Ferrer <gelferrero01@gmail.com>
2024-01-24 20:05:33 +0800

    User input - string

---
+++ exercise_three.py
@@ -0,0 +1,6 @@
+# Print characters from a string that are present at an even index number
+# Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# Accept string input from the user.
+string_input = input ("Please enter a word: ")
+print ("The original string is",string_input)
